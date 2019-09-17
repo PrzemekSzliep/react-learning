bb1a8416b8a6f475afe64830a165745fc1cf4bff
Przemek Szliep <szlipek@gmail.com>
2019-09-17 14:58:24 +0200

    props

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
-const Hero = () => (
+const Hero = props => (
     <section className={styles.component}>
-        <img src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" alt="" className={styles.image}/>
-        <h2 className={styles.title}>Things to do</h2>
+        <img src={props.image} alt="" className={styles.image}/>
+        <h2 className={styles.title}>{props.titleText}</h2>
     </section>
 );
+
+Hero.propTypes = {
+    titleText: PropTypes.node.isRequired,
+    image: PropTypes.string,
+}
 
 export default Hero;
