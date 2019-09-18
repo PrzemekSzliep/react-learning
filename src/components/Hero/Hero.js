eb47a9ef3dd74ae0704bd41c269919c2e2a24203
Przemek Szliep <szlipek@gmail.com>
2019-09-18 17:01:17 +0200

    cards

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Hero.scss';
 import PropTypes from 'prop-types';
+import ReactHtmlParser from 'react-html-parser';
 
 const Hero = props => (
     <section className={styles.component}>
         <img src={props.image} alt="" className={styles.image}/>
-        <h2 className={styles.title}>{props.titleText}</h2>
+        <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
     </section>
 );
 
